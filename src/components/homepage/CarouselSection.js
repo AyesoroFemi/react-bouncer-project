6765882bb772d3f45d3b4e8f39c2a7685e767821
Decagon <decagon@Femi-Decagons-MacBook-Air.local>
2021-10-22 16:10:32 +0100

    pic changed

--- src/components/homepage/CarouselSection.js
+++ src/components/homepage/CarouselSection.js
@@ -7,13 +7,13 @@ function CarouselSection() {
             <div className="container-hero">
                 <div className="hero-category">
                     <div>
-                        <img src="iphone_ad 1 (1).png" alt="Iphone_pic" />
+                        <img src="img/iphone_ad 1 (1).png" alt="Iphone_pic" />
                     </div>
                     <div>
-                        <img src="oculus_rift_ad_nrpsc2 1 (1).png" alt="object_pic" />
+                        <img src="img/oculus_rift_ad_nrpsc2 1 (1).png" alt="object_pic" />
                     </div>
                     <div>
-                        <img src="go_pro_ad_fumlul 1 (1).png" alt="object-image_pic" />
+                        <img src="img/go_pro_ad_fumlul 1 (1).png" alt="object-image_pic" />
                     </div>
                 </div>
             </div>
